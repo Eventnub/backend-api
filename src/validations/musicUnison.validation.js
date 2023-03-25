c6395bda5c55b04dcc911072309ff894d26619d4
ofojichigozie <ofojichigozie@gmail.com>
2023-03-25 08:51:32 +0100

    Added validations for music unison management

--- src/validations/musicUnison.validation.js
+++ src/validations/musicUnison.validation.js
@@ -1,10 +1,55 @@
 const Joi = require("joi");
 const { documentId } = require("./custom.validation");
 
+const createMusicUnison = {
+  body: Joi.object().keys({
+    eventId: Joi.string().required(),
+    songTranscript: Joi.string().required(),
+    songArtist: Joi.string().required(),
+    songTitle: Joi.string().required(),
+  }),
+  file: Joi.string().required(),
+};
+
+const getMusicUnison = {
+  params: Joi.object().keys({
+    uid: Joi.required().custom(documentId),
+  }),
+};
+
+const updateMusicUnison = {
+  params: Joi.object().keys({
+    uid: Joi.required().custom(documentId),
+  }),
+  body: Joi.object().keys({
+    songTranscript: Joi.string(),
+    songArtist: Joi.string(),
+    songTitle: Joi.string(),
+  }),
+  file: Joi.string(),
+};
+
+const deleteMusicUnison = {
+  params: Joi.object().keys({
+    uid: Joi.string().required().custom(documentId),
+  }),
+};
+
+const getEventMusicUnisons = {
+  params: Joi.object().keys({
+    eventId: Joi.required().custom(documentId),
+  }),
+};
+
 const transcribeAudio = {
   file: Joi.string().required(),
 };
 
 module.exports = {
+  createMusicUnison,
+  getMusicUnison,
+  updateMusicUnison,
+  deleteMusicUnison,
+  getEventMusicUnisons,
   transcribeAudio,
 };
