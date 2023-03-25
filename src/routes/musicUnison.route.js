b8136b9fe3b79978ecabe369405eb369c348f258
ofojichigozie <ofojichigozie@gmail.com>
2023-03-25 10:16:02 +0100

    Added route for music unison management

--- src/routes/musicUnison.route.js
+++ src/routes/musicUnison.route.js
@@ -2,9 +2,49 @@ const express = require("express");
 const { Authentication, Authorization } = require("../middlewares/auth");
 const multerConfig = require("../config/multer");
 const validate = require("../middlewares/validate");
+const musicUnisonValidation = require("../validations/musicUnison.validation");
 const musicUnisonController = require("../controllers/musicUnison.controller");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .post(
+    Authentication,
+    Authorization(["admin", "host"]),
+    multerConfig.single("audio"),
+    validate(musicUnisonValidation.createMusicUnison),
+    musicUnisonController.createMusicUnison
+  );
+
+router
+  .route("/:uid")
+  .get(
+    Authentication,
+    validate(musicUnisonValidation.getMusicUnison),
+    musicUnisonController.getMusicUnison
+  )
+  .patch(
+    Authentication,
+    Authorization(["admin", "host"]),
+    multerConfig.single("audio"),
+    validate(musicUnisonValidation.updateMusicUnison),
+    musicUnisonController.updateMusicUnison
+  )
+  .delete(
+    Authentication,
+    Authorization(["admin", "host"]),
+    validate(musicUnisonValidation.deleteMusicUnison),
+    musicUnisonController.deleteMusicUnison
+  );
+
+router
+  .route("/get-event-music-unisons/:eventId")
+  .get(
+    Authentication,
+    validate(musicUnisonValidation.getEventMusicUnisons),
+    musicUnisonController.getEventMusicUnisons
+  );
 
 router
   .route("/transcribe-audio")
@@ -12,6 +52,7 @@ router
     Authentication,
     Authorization(["admin", "host"]),
     multerConfig.single("audio"),
+    validate(musicUnisonValidation.transcribeAudio),
     musicUnisonController.transcribeAudio
   );
 
