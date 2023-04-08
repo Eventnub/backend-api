a921cb9bc7dbf4158c774fee438a02661a782453
ofojichigozie <ofojichigozie@gmail.com>
2023-04-09 00:10:52 +0100

    Integrated sinch

---
+++ src/services/sms.service.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const { sinchConfig } = require("../config/config");
+
+const sendSMS = async (to, message) => {
+  const response = await axios.post(
+    "https://us.sms.api.sinch.com/xms/v1/" +
+      sinchConfig.servicePlanId +
+      "/batches",
+    {
+      from: sinchConfig.sinchNumber,
+      to: [to],
+      body: message,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sinchConfig.apiToken,
+      },
+    }
+  );
+};
